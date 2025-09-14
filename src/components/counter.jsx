78db7bdd972d042afdd091d2ed09aeeae1103942
Shafee <shafee.broly@gmail.com>
2025-09-14 23:40:46 +0600

    Add Part er kaj

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,23 +1,45 @@
+// import { useState } from "react";
 import { useState } from "react";
+import initialArtist from "../data/artist_data";
 
 export default function Counter() {
-  const [count, setCount] = useState(0);
+  const [artists, setArtists] = useState(initialArtist);
 
-  function incriment() {
-    setCount((prevCoutn) => prevCoutn + 1);
-    setCount((prevCoutn) => prevCoutn + 1);
-    setCount((prevCoutn) => prevCoutn + 1);
-    // setCount(count + 1);
-    // setCount(count + 1);
-  }
+  // delet er jonno Function banacci (evabeo funcction banano jay)
+  const deleteArtist = (id) => {
+    console.log(id);
+    //
+  };
+
+  // add er jonno Function banacci
+  const addSumith = () => {
+    const sumith = { id: 4, name: "sumith" };
+    const newArrey = [...artists, sumith];
+    setArtists(newArrey);
+  };
+  // const [count, setCount] = useState(0);
+
+  // function incriment() {
+  // setCount((prevCoutn) => prevCoutn + 1);
+  // setCount((prevCoutn) => prevCoutn + 1);
+  // setCount((prevCoutn) => prevCoutn + 1);
+  // setCount(count + 1);
+  // setCount(count + 1);
+  // }
 
   return (
     <div>
-      <h1>Count: {count} </h1>
-
       <div>
-        <button onClick={incriment}>Counting</button>
+        <button onClick={addSumith}>Add Sumith the boss</button>
       </div>
+      <ul>
+        {artists.map((artists) => (
+          <li key={artists.id}>
+            {artists.name}{" "}
+            <button onClick={() => deleteArtist(artists.id)}>Delete</button>{" "}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
